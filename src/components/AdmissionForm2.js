550e0b31375c2e97ad841be43292783236573a7a
Arun <arunkumar.jaiswar@teamlease.com>
2025-04-26 10:21:52 +0530

    changes to resolved lib issue

--- src/components/AdmissionForm2.js
+++ src/components/AdmissionForm2.js
@@ -10,6 +10,8 @@ import {
   Grid
 } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 const AdmissionForm2 = () => {
   const [formData, setFormData] = useState({
@@ -45,188 +47,197 @@ const AdmissionForm2 = () => {
   };
 
   return (
-    <Paper elevation={3} sx={{ p: 4, maxWidth: 800, margin: 'auto', mt: 4 }}>
-      <Typography variant="h4" gutterBottom sx={{ mb: 4, textAlign: 'center' }}>
-        Student Admission Form
-      </Typography>
-      
-      <form onSubmit={handleSubmit}>
-        {/* Personal Information Section */}
-        <Box sx={{ mb: 4 }}>
-          <Typography variant="h6" gutterBottom sx={{ borderBottom: '1px solid #eee', pb: 1 }}>
-            Personal Information
-          </Typography>
-          
-          <Grid container spacing={3} sx={{ mt: 1 }}>
-            <Grid item xs={12} sm={6}>
-              <FormControlLabel
-                control={<Checkbox checked={!!formData.fullName} />}
-                label="Full Name"
-                sx={{ mb: 1 }}
-              />
-              <TextField
-                fullWidth
-                name="fullName"
-                value={formData.fullName}
-                onChange={handleChange}
-                placeholder="Enter your full name"
-                variant="outlined"
-              />
-            </Grid>
-            
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body1" sx={{ mb: 1 }}>Date of Birth</Typography>
-              <DatePicker
-                value={formData.dob}
-                onChange={handleDateChange}
-                renderInput={(params) => <TextField {...params} fullWidth />}
-              />
-            </Grid>
-          </Grid>
-        </Box>
 
-        {/* Government IDs Section */}
-        <Box sx={{ mb: 4 }}>
-          <Typography variant="h6" gutterBottom sx={{ borderBottom: '1px solid #eee', pb: 1 }}>
-            Government Identification
-          </Typography>
-          
-          <Grid container spacing={3} sx={{ mt: 1 }}>
-            <Grid item xs={12} sm={6}>
-              <FormControlLabel
-                control={<Checkbox checked={!!formData.aadharNumber} />}
-                label="Aadhar Number"
-                sx={{ mb: 1 }}
-              />
-              <TextField
-                fullWidth
-                name="aadharNumber"
-                value={formData.aadharNumber}
-                onChange={handleChange}
-                placeholder="Enter 12-digit Aadhar number"
-                variant="outlined"
-              />
-            </Grid>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Paper elevation={3} sx={{ p: 4, maxWidth: 800, margin: 'auto', mt: 4 }}>
+        <Typography variant="h4" gutterBottom sx={{ mb: 4, textAlign: 'center' }}>
+          Student Admission Form
+        </Typography>
+        <Paper elevation={3} sx={{ p: 4, maxWidth: 800, margin: 'auto', mt: 4 }}>
+            <Typography variant="h4" gutterBottom sx={{ mb: 4, textAlign: 'center' }}>
+                Student Admission Form
+            </Typography>
             
-            <Grid item xs={12} sm={6}>
-              <FormControlLabel
-                control={<Checkbox checked={!!formData.panNumber} />}
-                label="PAN Number"
-                sx={{ mb: 1 }}
-              />
-              <TextField
-                fullWidth
-                name="panNumber"
-                value={formData.panNumber}
-                onChange={handleChange}
-                placeholder="Enter PAN number"
-                variant="outlined"
-              />
-            </Grid>
-            
-            <Grid item xs={12} sm={6}>
-              <FormControlLabel
-                control={<Checkbox checked={!!formData.drivingLicense} />}
-                label="Driving License Number"
-                sx={{ mb: 1 }}
-              />
-              <TextField
-                fullWidth
-                name="drivingLicense"
-                value={formData.drivingLicense}
-                onChange={handleChange}
-                placeholder="Enter driving license number"
-                variant="outlined"
-              />
-            </Grid>
-          </Grid>
-        </Box>
+            <form onSubmit={handleSubmit}>
+                {/* Personal Information Section */}
+                <Box sx={{ mb: 4 }}>
+                <Typography variant="h6" gutterBottom sx={{ borderBottom: '1px solid #eee', pb: 1 }}>
+                    Personal Information
+                </Typography>
+                
+                <Grid container spacing={3} sx={{ mt: 1 }}>
+                    <Grid item xs={12} sm={6}>
+                    <FormControlLabel
+                        control={<Checkbox checked={!!formData.fullName} />}
+                        label="Full Name"
+                        sx={{ mb: 1 }}
+                    />
+                    <TextField
+                        fullWidth
+                        name="fullName"
+                        value={formData.fullName}
+                        onChange={handleChange}
+                        placeholder="Enter your full name"
+                        variant="outlined"
+                    />
+                    </Grid>
+                    
+                    <Grid item xs={12} sm={6}>
+                    <Typography variant="body1" sx={{ mb: 1 }}>Date of Birth</Typography>
+                    <DatePicker
+                        value={formData.dob}
+                        onChange={handleDateChange}
+                        renderInput={(params) => <TextField {...params} fullWidth />}
+                    />
+                    </Grid>
+                </Grid>
+                </Box>
 
-        {/* Contact Information Section */}
-        <Box sx={{ mb: 4 }}>
-          <Typography variant="h6" gutterBottom sx={{ borderBottom: '1px solid #eee', pb: 1 }}>
-            Contact Information
-          </Typography>
-          
-          <Grid container spacing={3} sx={{ mt: 1 }}>
-            <Grid item xs={12} sm={6}>
-              <FormControlLabel
-                control={<Checkbox checked={!!formData.email} />}
-                label="Email Address"
-                sx={{ mb: 1 }}
-              />
-              <TextField
-                fullWidth
-                name="email"
-                type="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Enter email address"
-                variant="outlined"
-              />
-            </Grid>
-            
-            <Grid item xs={12} sm={6}>
-              <FormControlLabel
-                control={<Checkbox checked={!!formData.contactNumber} />}
-                label="Contact Number"
-                sx={{ mb: 1 }}
-              />
-              <TextField
-                fullWidth
-                name="contactNumber"
-                value={formData.contactNumber}
-                onChange={handleChange}
-                placeholder="Enter contact number"
-                variant="outlined"
-              />
-            </Grid>
-          </Grid>
-        </Box>
+                {/* Government IDs Section */}
+                <Box sx={{ mb: 4 }}>
+                <Typography variant="h6" gutterBottom sx={{ borderBottom: '1px solid #eee', pb: 1 }}>
+                    Government Identification
+                </Typography>
+                
+                <Grid container spacing={3} sx={{ mt: 1 }}>
+                    <Grid item xs={12} sm={6}>
+                    <FormControlLabel
+                        control={<Checkbox checked={!!formData.aadharNumber} />}
+                        label="Aadhar Number"
+                        sx={{ mb: 1 }}
+                    />
+                    <TextField
+                        fullWidth
+                        name="aadharNumber"
+                        value={formData.aadharNumber}
+                        onChange={handleChange}
+                        placeholder="Enter 12-digit Aadhar number"
+                        variant="outlined"
+                    />
+                    </Grid>
+                    
+                    <Grid item xs={12} sm={6}>
+                    <FormControlLabel
+                        control={<Checkbox checked={!!formData.panNumber} />}
+                        label="PAN Number"
+                        sx={{ mb: 1 }}
+                    />
+                    <TextField
+                        fullWidth
+                        name="panNumber"
+                        value={formData.panNumber}
+                        onChange={handleChange}
+                        placeholder="Enter PAN number"
+                        variant="outlined"
+                    />
+                    </Grid>
+                    
+                    <Grid item xs={12} sm={6}>
+                    <FormControlLabel
+                        control={<Checkbox checked={!!formData.drivingLicense} />}
+                        label="Driving License Number"
+                        sx={{ mb: 1 }}
+                    />
+                    <TextField
+                        fullWidth
+                        name="drivingLicense"
+                        value={formData.drivingLicense}
+                        onChange={handleChange}
+                        placeholder="Enter driving license number"
+                        variant="outlined"
+                    />
+                    </Grid>
+                </Grid>
+                </Box>
 
-        {/* Photo Upload Section */}
-        <Box sx={{ mb: 4 }}>
-          <Typography variant="h6" gutterBottom sx={{ borderBottom: '1px solid #eee', pb: 1 }}>
-            Photo Upload
-          </Typography>
-          
-          <Box sx={{ mt: 2 }}>
-            <FormControlLabel
-              control={<Checkbox checked={!!formData.photo} />}
-              label="Upload Photo"
-              sx={{ mb: 1 }}
-            />
-            <Button
-              variant="outlined"
-              component="label"
-              fullWidth
-              sx={{ textTransform: 'none' }}
-            >
-              {formData.photo ? formData.photo.name : 'Choose File'}
-              <input
-                type="file"
-                name="photo"
-                hidden
-                onChange={handleChange}
-                accept="image/*"
-              />
-            </Button>
-          </Box>
-        </Box>
+                {/* Contact Information Section */}
+                <Box sx={{ mb: 4 }}>
+                <Typography variant="h6" gutterBottom sx={{ borderBottom: '1px solid #eee', pb: 1 }}>
+                    Contact Information
+                </Typography>
+                
+                <Grid container spacing={3} sx={{ mt: 1 }}>
+                    <Grid item xs={12} sm={6}>
+                    <FormControlLabel
+                        control={<Checkbox checked={!!formData.email} />}
+                        label="Email Address"
+                        sx={{ mb: 1 }}
+                    />
+                    <TextField
+                        fullWidth
+                        name="email"
+                        type="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        placeholder="Enter email address"
+                        variant="outlined"
+                    />
+                    </Grid>
+                    
+                    <Grid item xs={12} sm={6}>
+                    <FormControlLabel
+                        control={<Checkbox checked={!!formData.contactNumber} />}
+                        label="Contact Number"
+                        sx={{ mb: 1 }}
+                    />
+                    <TextField
+                        fullWidth
+                        name="contactNumber"
+                        value={formData.contactNumber}
+                        onChange={handleChange}
+                        placeholder="Enter contact number"
+                        variant="outlined"
+                    />
+                    </Grid>
+                </Grid>
+                </Box>
 
-        {/* Submit Button */}
-        <Box sx={{ textAlign: 'center', mt: 4 }}>
-          <Button 
-            type="submit" 
-            variant="contained" 
-            size="large"
-            sx={{ px: 6, py: 1.5 }}
-          >
-            Submit Application
-          </Button>
-        </Box>
-      </form>
-    </Paper>
+                {/* Photo Upload Section */}
+                <Box sx={{ mb: 4 }}>
+                <Typography variant="h6" gutterBottom sx={{ borderBottom: '1px solid #eee', pb: 1 }}>
+                    Photo Upload
+                </Typography>
+                
+                <Box sx={{ mt: 2 }}>
+                    <FormControlLabel
+                    control={<Checkbox checked={!!formData.photo} />}
+                    label="Upload Photo"
+                    sx={{ mb: 1 }}
+                    />
+                    <Button
+                    variant="outlined"
+                    component="label"
+                    fullWidth
+                    sx={{ textTransform: 'none' }}
+                    >
+                    {formData.photo ? formData.photo.name : 'Choose File'}
+                    <input
+                        type="file"
+                        name="photo"
+                        hidden
+                        onChange={handleChange}
+                        accept="image/*"
+                    />
+                    </Button>
+                </Box>
+                </Box>
+
+                {/* Submit Button */}
+                <Box sx={{ textAlign: 'center', mt: 4 }}>
+                <Button 
+                    type="submit" 
+                    variant="contained" 
+                    size="large"
+                    sx={{ px: 6, py: 1.5 }}
+                >
+                    Submit Application
+                </Button>
+                </Box>
+            </form>
+            </Paper>
+      </Paper>
+    </LocalizationProvider>
+    
   );
 };
 
